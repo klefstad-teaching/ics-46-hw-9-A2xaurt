daababdae62e676c0af1825dec7201cf008e6adb
A2xaurt <A2xaurt@gmail.com>
2025-03-09 19:12:34 +0000

    Updated testing for generate_word_ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -104,12 +104,15 @@ vector<string> generate_word_ladder(const string &begin_word, const string &end_
         {
             if(is_adjacent(last_word,word))
             {
-                visited.insert(word);
-                vector<string> new_ladder=ladder;
-                new_ladder.push_back(word);
-                if(word==end_word)
-                    return new_ladder;
-                ladder_queue.push(new_ladder);
+                if(!visited.contains(word))
+                {
+                    visited.insert(word);
+                    vector<string> new_ladder=ladder;
+                    new_ladder.push_back(word);
+                    if(word==end_word)
+                        return new_ladder;
+                    ladder_queue.push(new_ladder);
+                }
             }
         }
     }
