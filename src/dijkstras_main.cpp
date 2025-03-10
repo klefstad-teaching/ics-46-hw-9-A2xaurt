f3f2d96a34a75dc058fde5a4b63c646f4f6dad2d
A2xaurt <A2xaurt@gmail.com>
2025-03-10 00:46:59 +0000

    Made and tested dijkstra's main

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,6 +1,15 @@
 //dijkstras_main.cpp
+#include "dijkstras.h"
 
 int main()
 {
-    return 0;
+    Graph G;
+    file_to_graph("src/smallest.txt",G);
+    vector<int> previous(G.numVertices);
+    vector<int> distances=dijkstra_shortest_path(G,0,previous);
+    for(int i=0; i<G.numVertices; ++i)
+    {
+        vector<int> shortest_path=extract_shortest_path(distances,previous,i);
+        print_path(shortest_path,distances[i]);
+    }
 }
