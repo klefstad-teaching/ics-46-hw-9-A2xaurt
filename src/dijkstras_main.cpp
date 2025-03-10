fa6f8eafcc36a59cc9c86e74e687878833e89ac8
A2xaurt <A2xaurt@gmail.com>
2025-03-10 00:48:53 +0000

    Updated disjktra's main

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     Graph G;
-    file_to_graph("src/smallest.txt",G);
+    file_to_graph("src/small.txt",G);
     vector<int> previous(G.numVertices);
     vector<int> distances=dijkstra_shortest_path(G,0,previous);
     for(int i=0; i<G.numVertices; ++i)
